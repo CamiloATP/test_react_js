4f3609959d88501b8f048d5092f3241eff00bf60
Camilo <camilo.antonio.t@gmail.com>
2019-02-07 22:10:26 -0300

    Conociendo component y status

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -6,21 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+      <nav>
+        <h1>Welcome to React.js</h1>
+      </nav>
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Welcome to React.js</h1>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
       </div>
     );
   }
--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,10 +1,24 @@
+// Primero se importa react
 import React from 'react';
+// Se importa ReactDOM el cual esta enfocado en el navegador
 import ReactDOM from 'react-dom';
+// React.js trabaja con webpack tecnologia de empaquetado para importar archivos
+// css dentro de un js
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Se esta importando una aplicación desde App.js
+import App from './App';
+
+// Para las app que simulan datos de manera local (para guardar los datos en el
+// navegador o computador de manera offline)
+// progressive web app <-- investigar
+import * as serviceWorker from './serviceWorker'; // para que la app trabaje sin conexión
+
+// app de navegador renderizar la app en pantalla
+ReactDOM.render(
+  <App />, // <-- la App importada jsx de la app
+  document.getElementById('root') // <-- donde va a cargar la app
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
